0ab815b362cb1ca01435325a03bcc916a4b90eaf
Skyler Tolman <skyler237@gmail.com>
2018-01-16 15:33:29 -0700

    Added basic UAV teleop script

---
+++ myholodeck/uav_teleop.py
@@ -0,0 +1,180 @@
+import numpy as np
+import pygame
+from pygame.locals import *
+import math
+import curses
+
+from Holodeck import Holodeck, Agents
+from Holodeck.Environments import HolodeckEnvironment
+from Holodeck.Sensors import Sensors
+
+
+TIME_OUT_VAL = 1000
+
+# Command key mappings
+ROLL_RIGHT  = K_d
+ROLL_LEFT   = K_a
+PITCH_UP    = K_w
+PITCH_DOWN  = K_s
+YAW_LEFT    = K_LEFT
+YAW_RIGHT   = K_RIGHT
+ALT_UP      = K_UP
+ALT_DOWN    = K_DOWN
+SPEED_UP    = K_e
+SPEED_DOWN  = K_q
+QUIT = K_ESCAPE
+# ROLL_RIGHT  = ord('d')
+# ROLL_LEFT   = ord('a')
+# PITCH_UP    = ord('w')
+# PITCH_DOWN  = ord('s')
+# YAW_LEFT    = ord('j')
+# YAW_RIGHT   = ord('l')
+# ALT_UP      = ord('i')
+# ALT_DOWN    = ord('k')
+# QUIT        = 27
+
+
+# This is a basic example of how to use the UAV agent
+def uav_example():
+    pygame.init()
+    screen = pygame.display.set_mode( (640,480) )
+    pygame.display.set_caption('Python numbers')
+
+    font = pygame.font.Font(None, 50)
+    text = "nothing yet..."
+    # pygame.init()
+    # # pygame.event.set_grab(True)
+
+    # stdscr = curses.initscr()
+    # curses.cbreak()
+    # stdscr.keypad(1)
+
+    env = Holodeck.make("UrbanCity")
+
+    # Initialize watchdog timer`
+    timeout_cnt = TIME_OUT_VAL
+
+    # Initialize altitude to zero
+    alt_c = 0
+    speed_val = 0.0
+
+    # Rate parameters
+    roll_min = math.radians(10)
+    roll_max = math.radians(45)
+    pitch_min = math.radians(10)
+    pitch_max = math.radians(45)
+    yawrate_min = math.radians(30)
+    yawrate_max = math.radians(90)
+    altrate_min = 0.1
+    altrate_max = 0.5
+    speed_min = 0.0
+    speed_max = 1.0
+    speed_rate = 0.1
+
+    # Loop forever
+    while True:
+        timeout_cnt -= 1
+        # Default all angles/rates to zero at each time step
+        roll_c = 0
+        pitch_c = 0
+        yawrate_c = 0
+        # Check keyboard input
+
+        pygame.event.pump()
+        keys=pygame.key.get_pressed()
+        # key = stdscr.getch()
+
+        # Update control values
+        if keys[K_ESCAPE] or timeout_cnt < 0:
+            break # Quit the program
+        if keys[ROLL_RIGHT]:
+            timeout_cnt = TIME_OUT_VAL
+            roll_c = -(roll_min + (roll_max - roll_min)*speed_val)
+            text = "ROLL_RIGHT"
+        if keys[ROLL_LEFT]:
+            timeout_cnt = TIME_OUT_VAL
+            roll_c = (roll_min + (roll_max - roll_min)*speed_val)
+            text = "ROLL_LEFT"
+        if keys[PITCH_UP]:
+            timeout_cnt = TIME_OUT_VAL
+            pitch_c = (pitch_min + (pitch_max - pitch_min)*speed_val)
+            text = "PITCH_UP"
+        if keys[PITCH_DOWN]:
+            timeout_cnt = TIME_OUT_VAL
+            pitch_c = -(pitch_min + (pitch_max - pitch_min)*speed_val)
+            text = "PITCH_DOWN"
+        if keys[YAW_LEFT]:
+            timeout_cnt = TIME_OUT_VAL
+            yawrate_c = (yawrate_min + (yawrate_max - yawrate_min)*speed_val)
+            text = "YAW_LEFT"
+        if keys[YAW_RIGHT]:
+            timeout_cnt = TIME_OUT_VAL
+            yawrate_c = -(yawrate_min + (yawrate_max - yawrate_min)*speed_val)
+            text = "YAW_RIGHT"
+        if keys[ALT_UP]:
+            timeout_cnt = TIME_OUT_VAL
+            alt_c += (altrate_min + (altrate_max - altrate_min)*speed_val)
+            text = "Altitude raised to {0}".format(alt_c)
+        if keys[ALT_DOWN]:
+            timeout_cnt = TIME_OUT_VAL
+            alt_c -= max(((altrate_min + (altrate_max - altrate_min)*speed_val), 0))
+            text = "Altitude lowered to {0}".format(alt_c)
+        if keys[SPEED_UP]:
+            timeout_cnt = TIME_OUT_VAL
+            speed_val += speed_rate
+            speed_val = min(speed_val, speed_max)
+            text = "Speed raised to {0}".format(speed_val)
+        if keys[SPEED_DOWN]:
+            timeout_cnt = TIME_OUT_VAL
+            speed_val -= speed_rate
+            speed_val = max(speed_val, speed_min)
+            text = "Speed lowered to {0}".format(speed_val)
+        # if key == K_ESCAPE or timeout_cnt < 0:
+        #     break # Quit the program
+        # if key == ROLL_RIGHT:
+        #     timeout_cnt = TIME_OUT_VAL
+        #     roll_c = roll_min
+        # if key == ROLL_LEFT:
+        #     timeout_cnt = TIME_OUT_VAL
+        #     roll_c = -roll_min
+        # if key == PITCH_UP:
+        #     timeout_cnt = TIME_OUT_VAL
+        #     pitch_c = pitch_min
+        # if key == PITCH_DOWN:
+        #     timeout_cnt = TIME_OUT_VAL
+        #     pitch_c = -pitch_min
+        # if key == YAW_LEFT:
+        #     timeout_cnt = TIME_OUT_VAL
+        #     yawrate_c = -yawrate_min
+        # if key == YAW_RIGHT:
+        #     timeout_cnt = TIME_OUT_VAL
+        #     yawrate_c = yawrate_min
+        # if key == ALT_UP:
+        #     timeout_cnt = TIME_OUT_VAL
+        #     alt_c += altrate_min
+        # if key == ALT_DOWN:
+        #     timeout_cnt = TIME_OUT_VAL
+        #     alt_c -= max(altrate_min, 0)
+
+        # text = "key pressed!"
+        screen.fill((0,0,0))
+        block = font.render(text, True, (255,255,255))
+        rect = block.get_rect()
+        rect.center = screen.get_rect().center
+        screen.blit(block, rect)
+        pygame.display.flip()
+
+
+        # Construct command
+        command = np.array([roll_c, pitch_c, yawrate_c, alt_c])
+
+        # Step simulator
+        state, reward, terminal, info = env.step(command)
+
+        # Could access sensor data here
+
+
+
+if __name__ == "__main__":
+    uav_example()
+    print("Finished")
