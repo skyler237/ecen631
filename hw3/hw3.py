5acd1ef190c15639a076a1cfb6122c6abaeea665
Skyler Tolman <skyler237@gmail.com>
2018-01-23 15:18:24 -0700

    Fixed python2 vs. py3 path issues

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -1,4 +1,10 @@
 import sys
+rm_python2 = []
+for p in sys.path:
+    if p.find('python2') != -1:
+        rm_python2.append(p)
+for p in rm_python2:
+    sys.path.remove(p)
 sys.path.insert(0, '/home/skyler/school/ecen631/myholodeck')
 
 import cv2
