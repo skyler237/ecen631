34593346d6297218170f8c9aa9a50e383f510554
Unknown <skyler237@gmail.com>
2018-02-20 15:30:56 -0700

    Implemented weighted average -- needs debugging

--- my_cv/target_tracker.py
+++ my_cv/target_tracker.py
@@ -9,23 +9,24 @@ from my_cv.multi_image import MultiImage
 ############ Main target tracker ############
 class TargetTracker:
     def __init__(self, tracker_type="KLT"):
+        dt = 1/30.0
         # Initialize tracker
         tracker_types = {"KLT": KLTTracker,
                          "MeanShift": MeanShiftTracker,
                          "BGSubtraction": BGSubtractionTracker}
         self.tracker_type = tracker_types[tracker_type]
-        self.tracker = self.tracker_type()
+        self.tracker = self.tracker_type(dt=dt)
         self.roi_width = 0
         self.roi_height = 0
 
         # Initialize filter
         self.filter_model = Filter.CONST_VEL
-        self.filter = Filter(self.filter_model)
+        self.filter = Filter(self.filter_model, dt=dt)
 
     def track_targets(self, frame):
         measurements = self.tracker.get_measurements(frame)
         avg_meas = self.weighted_average(measurements)
-        self.filter.correct(meas)
+        self.filter.correct(avg_meas)
         filtered_meas = self.filter.predict()
         # set_trace()
         region_center = [filtered_meas[0], filtered_meas[1]]
@@ -34,11 +35,22 @@ class TargetTracker:
         self.tracker.display('Target Tracker')
 
     def weighted_average(self, measurements):
+        # Reshape the measurements to be column vectors
+        measurements = np.reshape(measurements, (len(measurements),4,1))
+
         ### Find the mahalanobis distance of each measurement
         # Get innovation cov matrix
-
-        # CODE HERE -- define this function below
         S = self.filter.get_S_matrix()
+        # Get current estimated state
+        xhat = self.filter.kalman.statePre
+
+        D = [[math.sqrt(np.matmul(np.matmul(np.transpose(y - xhat),np.linalg.inv(S)),(y - xhat)))] for y in measurements]
+
+        weighted_meas = np.multiply(D,measurements)
+        weighted_avg = np.sum(weighted_meas,0)/np.sum(D,1)
+
+        return weighted_avg
+
 
     def select_roi(self, frame):
         region = cv2.selectROI(frame)
@@ -55,28 +67,27 @@ class Filter:
     CONST_VEL = 1
     CONST_ACCEL = 2
     CONST_JERK = 3
-    def __init__(self, model_type=CONST_VEL):
-        self.dt = 1.0/30.0
+    def __init__(self, model_type=CONST_VEL, dt=1/30.0):
+        self.dt = dt
         self.model_type = model_type
 
-        self.meas_dim = 2
+        self.meas_dim = 4
         self.sigma_R  = 1.0
         if model_type == self.CONST_VEL:
-            self.state_dim = self.meas_dim*2
+            self.state_dim = 4
             self.sigma_Q = 5.0
         elif model_type == self.CONST_ACCEL:
-            self.state_dim = self.meas_dim*3
+            self.state_dim = 6
             self.sigma_Q = 1.0
         elif model_type == self.CONST_JERK:
-            self.state_dim = self.meas_dim*4
+            self.state_dim = 8
             self.sigma_Q = 0.5
 
         data_type = np.float32
         self.kalman = cv2.KalmanFilter(self.state_dim,self.meas_dim)
 
         # Matrix definitions
-        self.kalman.measurementMatrix   = np.array([[1,0,0,0],
-                                                    [0,1,0,0]]).astype(data_type)
+        self.kalman.measurementMatrix   = self.get_C_matrix().astype(data_type)
         self.kalman.transitionMatrix    = self.get_A_matrix().astype(data_type)
         self.kalman.processNoiseCov     = self.get_Q_matrix().astype(data_type)
         self.kalman.measurementNoiseCov = self.sigma_R*np.eye(self.meas_dim).astype(data_type)
@@ -84,7 +95,7 @@ class Filter:
         # Initial conditions
         P_init = self.sigma_R # We use the first measurement to initialize the state
         self.kalman.errorCovPost = P_init*np.eye(self.state_dim).astype(data_type)
-        # self.kalman.errorCovPre = np.eye(self.state_dim).astype(data_type)
+        self.kalman.errorCovPre = self.kalman.errorCovPost
         self.state_init = False
 
     def get_A_matrix(self):
@@ -111,6 +122,21 @@ class Filter:
                           [1.0/24 *dt**4, 1.0/6 *dt**3, 1.0/2 *dt**2,        dt   ]])
         return self.sigma_Q*np.kron(Q, np.eye(2))
 
+    def get_C_matrix(self):
+        I = np.eye(self.meas_dim)
+        zeros = np.zeros((self.meas_dim, self.state_dim - self.meas_dim))
+        C = np.hstack((I,zeros))
+
+        return C
+
+    def get_S_matrix(self):
+        P = self.kalman.errorCovPre
+        C = self.kalman.measurementMatrix
+        R = self.kalman.measurementNoiseCov
+
+        S = np.matmul(np.matmul(C,P),C.transpose()) + R
+        return S
+
     def predict(self):
         return self.kalman.predict()
 
@@ -119,29 +145,27 @@ class Filter:
         if len(meas) == 0:
             return
 
-        # Reshape the measurement in to a column vector
-        meas = np.array(meas, np.float32).reshape((self.meas_dim,1))
-
         # Initialize state with first measurement
         if not self.state_init:
             # set_trace()
-            self.kalman.statePost = np.vstack((meas,[[0.],[0.]])).astype(np.float32)
-            self.kalman.statePre = np.vstack((meas,[[0.],[0.]])).astype(np.float32)
+            self.kalman.statePost = meas.astype(np.float32)
+            self.kalman.statePre = meas.astype(np.float32)
             self.state_init = True
 
-        return self.kalman.correct(meas)
+        return self.kalman.correct(meas.astype(np.float32))
 
 
 ############ Individual tracker implementations ############
 class Tracker:
     ''' Template class for tracking in an image
     '''
-    def __init__(self):
+    def __init__(self, dt=1/30.0):
         # Default image parameters
         self.default_img_size = (480,720,3)
         self.default_img_type = 'bgr'
 
         # Default variables
+        self.dt = dt
         self.tracks_mask = np.zeros(self.default_img_size, dtype=np.uint8)
         self.display_img = np.zeros(self.default_img_size, dtype=np.uint8)
         # Create empty grayscale region of interest mask
@@ -174,9 +198,9 @@ class Tracker:
 
 
 class KLTTracker(Tracker):
-    def __init__(self, max_features=1):
+    def __init__(self, max_features=1, dt=1/30.0):
         # Inherit fram Tracker init
-        super().__init__()
+        super().__init__(dt=dt)
 
         # Initialize parameters
         self.feature_params = dict( maxCorners = max_features,
@@ -215,14 +239,22 @@ class KLTTracker(Tracker):
         self.new_features = self.new_features[status==1]
         self.features = self.features[status==1]
 
+        # Calculate optical flow velocity
+        u = (self.new_features - self.features)/self.dt
+        u = np.reshape(u, (len(u), 2,1))
+
         # Update display
         self.update_display(frame)
 
+
         # Update previous data
         self.prev_gray = gray.copy()
-        self.features = self.new_features.reshape(-1,1,2)
+        self.features = self.new_features.reshape(-1,2,1)
 
-        return self.new_features
+        # Concatenate positions and velocities to create measurements
+        meas = np.hstack((self.features,u))
+
+        return meas
 
     def update_display(self, frame):
         for i,(new,old) in enumerate(zip(self.new_features,self.features)):
